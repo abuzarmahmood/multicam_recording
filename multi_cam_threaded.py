728702afcbac451ff04f77b33a5c85799704c551
abuzarmahmood <abuzarmahmood@gmail.com>
2019-06-03 16:59:32 -0400

    Moving window to keep frame rate under control

--- multi_cam_threaded.py
+++ multi_cam_threaded.py
@@ -18,6 +18,8 @@ class webcam_recording:
     in_count = 0
     out_count = 0
     time_list = []
+    moving_window = 100
+    
     
     def __init__(self,duration,frame_rate, cam_num, resolution = (640,480)):
         self.duration = duration # in seconds
@@ -25,6 +27,7 @@ class webcam_recording:
         self.total_frames = duration*frame_rate
         self.cam_num = cam_num
         self.resolution = resolution
+        
         
     def initialize_cameras(self):
         self.all_cams = [VideoStream(src = i).start() for i in range(self.cam_num)]
@@ -39,7 +42,12 @@ class webcam_recording:
             
     def read_frames(self):
         for count in range(self.total_frames):
-            time.sleep(1/self.frame_rate)
+            if self.in_count > self.moving_window:
+                next_rate = 1/((1/self.frame_rate)*self.moving_window -  \
+                            np.sum(np.diff(self.time_list[-self.moving_window:])))
+            else:
+                next_rate = self.frame_rate
+            time.sleep(1/next_rate)
             for cam in range(self.cam_num):
                 self.all_buffers[cam].append(self.all_cams[cam].read())
             self.in_count += 1
@@ -55,9 +63,9 @@ class webcam_recording:
             self.out_count += 1
                     
     def recording_stats(self):
-        time.sleep(0.1)
         print('Frame lag = ' + str(self.in_count-self.out_count) + \
-              ', Avg FR = ' + str(np.mean(np.diff(self.time_list[-20:]))))
+              ', Avg FR = ' + str(np.mean(np.diff(self.time_list[-self.moving_window:]))) + \
+              ', Total time = ' + str(self.time_list[-1] - self.time_list[0]))
                     
     def start_read(self):
         t = threading.Thread(target = self.read_frames, name='read_thread', args=())
@@ -67,12 +75,6 @@ class webcam_recording:
     
     def start_write(self):
         t = threading.Thread(target = self.write_frames, name='print_thread', args=())
-        t.start()
-        return self
-    
-    def show_stats(self):
-        t = threading.Thread(target = self.recording_stats, name='stats_thread', args=())
-        t.daemon = True
         t.start()
         return self
     
