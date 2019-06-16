c9f122e7f1a933395548e1df4f0009c74ca09b56
abuzarmahmood <abuzarmahmood@gmail.com>
2019-06-16 12:01:51 -0400

    Added folder making to execute file

--- multi_cam_threaded_execute.py
+++ multi_cam_threaded_execute.py
@@ -9,10 +9,23 @@ from multi_cam_threaded import webcam_recording
 import pylab as plt
 import numpy as np
 import datetime
+import sys
+import os
 
+folder_name = os.getcwd()
+file_name = sys.argv[1]+ '_' + \
+            datetime.datetime.today().\
+            strftime('%y%m%d-%H%M%S')
+
+fin_direc = (folder_name + '/' + file_name)
+if not os.path.exists(fin_direc):
+    os.mkdir(fin_direc)
 #this_recording = webcam_recording(10,30,2, resolution = (1280,720))
-this_recording = webcam_recording(10,30,2)
-
+this_recording = webcam_recording(
+                                duration = 2,
+                                frame_rate = 30,
+                                cam_num = 3,
+                                file_name = fin_direc + '/' + file_name) 
 # =============================================================================
 #this_recording.getDevices()
 #this_recording.initialize_cameras()
