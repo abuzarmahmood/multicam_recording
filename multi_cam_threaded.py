7cafe972f216490073f98d4099c81f8429335b66
abuzarmahmood <abuzarmahmood@gmail.com>
2019-07-17 14:32:08 -0400

    Current working version

--- multi_cam_threaded.py
+++ multi_cam_threaded.py
@@ -6,21 +6,22 @@ Created on Mon Jun  3 11:47:53 2019
 @author: abuzarmahmood
 """
 
+from imutils.video import VideoStream
+import numpy as np
+import time
+import cv2
+import pylab as plt
 import threading
 import os
 import re
-import time
-import cv2
-import numpy as np
-from imutils.video import VideoStream
-import pylab as plt
 
 class webcam_recording:
+    
     time_list = []
     moving_window = 100
     read_bool = 1
-    write_bool = 1
-    buffer_bool = 0
+    write_bool = 1    
+    time_bool = 0
  
     def __init__(self,
                 duration,
@@ -97,59 +98,28 @@ class webcam_recording:
                 self.all_buffers[cam].append(self.all_cams[cam].read())
                 self.in_count[cam] += 1
             self.time_list.append(time.time())
-            self.buffer_bool = 1        
+            self.time_bool = 1        
         self.read_bool = 0
-
-    def playFromBuffer(self):
-        fig, ax_list = plt.subplots(1,self.cam_num)
-        vid_list = [this_ax.imshow(this_pic,origin='lower')\
-                for this_pic,this_ax in \
-                zip([buffer[0] for buffer in self.all_buffers],
-                    ax_list)]
-        #fig.canvas.draw()
-        plt.ion()
-        plt.show(block=False)
-        for frame in range(len(self.all_buffers[0])):
-            for vid in range(len(vid_list)):
-                vid_list[vid].set_data(self.all_buffers[vid][frame])
-            fig.canvas.draw()
-            time.sleep(1/self.frame_rate)
-
+ 
     def write_frames(self):
-        # Define inner methods to simplify flow of main statement
-        def writeAllCams(self):
+        while self.write_bool > 0 or self.read_bool > 0:
+            time.sleep(0.5/self.frame_rate)
             for cam in range(self.cam_num):
-                self.all_writers[cam].write(self.all_buffers[cam][0])
-                self.all_buffers[cam].pop(0)
-                self.out_count[cam] += 1
-       
-        def writeTimeList(self):
-            with open("{0}_time_list.txt".format(self.file_name),"a") \
-                    as out_file:
-                out_file.write(str(self.time_list[self.out_count[0]-1]) + '\n')       
-        
-        def calcWriteBool(self):
+                if len(self.all_buffers[cam]) > 0:
+                    self.all_writers[cam].write(self.all_buffers[cam][0])
+                    self.all_buffers[cam].pop(0)
+                    self.out_count[cam] += 1
+             
             self.write_bool = \
                 sum([out_count < in_count for (out_count, in_count) in \
                      zip(self.out_count, self.in_count)])
-       
-        while True:
-            time.sleep(2/self.frame_rate)
-            if self.read_bool:
-                if self.buffer_bool:
-                    writeAllCams(self)
-                    writeTimeList(self)
-                    self.buffer_bool = 0
+            
+            if self.time_bool == 1:
+                with open("{0}_time_list.txt".format(self.file_name),"a") \
+                        as out_file:
+                    out_file.write(str(self.time_list[-1]) + '\n')        
+                self.time_bool = 0
 
-            else:
-                calcWriteBool(self)
-                if self.write_bool:
-                    writeAllCams(self)
-                    writeTimeList(self)
-                else:
-                    break
-
-                 
     def print_stats(self):
         print(
                 'Frame lag = {0}, Avg FR = {1}, , Total time = {2}'.format(
@@ -157,12 +127,7 @@ class webcam_recording:
                   str(np.mean(np.diff(self.time_list[-self.moving_window:]))),
                   str(self.time_list[-1] - self.time_list[0])
                   ))
-    
-    def output_time(self):
-        with open("time_list.txt","a") as out_file:
-            for time_point in self.time_list:
-                out_file.write(str(time_point) + '\n')        
-                 
+                     
     def start_read(self):
         t = threading.Thread(target = self.read_frames, name='read_thread', args=())
         t.daemon = True
@@ -183,6 +148,5 @@ class webcam_recording:
         self.initialize_writers()
         self.start_read()
         self.write_frames()
-       # self.output_time()
         self.shut_down()
         self.print_stats()
