eda9c5cac3cdcda3fc474ccabcf7944204bb8d72
abuzarmahmood <abuzarmahmood@gmail.com>
2019-07-30 11:53:32 -0400

    Working version | Takes input from stdin

--- split_code/split_script.py
+++ split_code/split_script.py
@@ -1,5 +1,29 @@
+# ___       _ _   _       _ _         
+#|_ _|_ __ (_) |_(_) __ _| (_)_______ 
+# | || '_ \| | __| |/ _` | | |_  / _ \
+# | || | | | | |_| | (_| | | |/ /  __/
+#|___|_| |_|_|\__|_|\__,_|_|_/___\___|
+#                                     
 
-## Import modules 
+import argparse
+
+parser = argparse.ArgumentParser(description = 'Collects names of files to process')
+parser.add_argument('marker_file', help = 'File containing start and end times of file')
+parser.add_argument('triallist_file', help = 'File containing timestamp of each trial')
+parser.add_argument('video_file', help = 'All video files to process', nargs = '+')
+args = parser.parse_args()
+
+print('\n')
+print('Marker file : {}'.format(args.marker_file))
+print('Trial list file: {}'.format(args.triallist_file))
+print('Video files : {}'.format(args.video_file))
+
+# ___                            _    
+#|_ _|_ __ ___  _ __   ___  _ __| |_  
+# | || '_ ` _ \| '_ \ / _ \| '__| __| 
+# | || | | | | | |_) | (_) | |  | |_  
+#|___|_| |_| |_| .__/ \___/|_|   \__| 
+#              |_|                    
 
 import cv2
 import glob
@@ -8,16 +32,33 @@ import numpy as np
 import pylab as plt
 from tqdm import tqdm
 import os
+
+# ____       _               
+#/ ___|  ___| |_ _   _ _ __  
+#\___ \ / _ \ __| | | | '_ \ 
+# ___) |  __/ |_| |_| | |_) |
+#|____/ \___|\__|\__,_| .__/ 
+#                     |_|    
+
+# Define functions to be used
+
+def get_total_frames(filename):
+    return cv2.VideoCapture(filename).get(7)
+
+def read_timelist(filename):
+    """
+    Define list reader
+    """
+    with open(filename,'r') as file:
+        timelist = [float(line) for line in file]
+    return timelist
+
 # Initialize filenames
 
-#directory = '/media/bigdata/blech_codes/vid_recordings/BS46_Extra_190618-110823'
-#directory = '/media/bigdata/blech_codes/vid_recordings/BS49_4tastes_190617-151511'
-directory = os.getcwd()
-#video_files = glob.glob( directory + '/**/*.avi', recursive=True)
-video_files = glob.glob( directory + '/**/*out*', recursive=True)
-#framelist_file = glob.glob( directory + '/**/*framelist*', recursive=True)[0]
-marker_file = glob.glob(directory + '/**/*markers*', recursive = True)[0]
-trial_list_file = glob.glob( directory + '/**/*trial_timelist*', recursive=True)[0]
+video_files = args.video_file
+marker_file = args.marker_file 
+triallist_file = args.triallist_file
+directory = os.path.dirname(os.path.abspath(marker_file))
 
 # Initialize Paramteres
 frame_rate = 30.0
@@ -29,79 +70,67 @@ t_post = 5
 t_prior_frames = np.ceil(t_prior * frame_rate).astype('int')
 t_post_frames = np.ceil(t_post * frame_rate).astype('int')
 
-
-def read_timelist(filename):
-    """
-    Define list reader
-    """
-    with open(filename,'r') as file:
-        timelist = [float(line) for line in file]
-    return timelist
-
 # Load list files
 #frame_list = read_timelist(framelist_file)
 marker_list = read_timelist(marker_file)
-trial_list = read_timelist(trial_list_file)
+trial_list = read_timelist(triallist_file)
 
 # Get total number of frames from file
-cam = 0
-video_name = video_files[cam]
-cap = cv2.VideoCapture(video_name)
-del cap
-frame_count = cap.get(7)
-frame_times = np.linspace(marker_list[0],marker_list[1],frame_count)
-frame_inds = [np.argmin(np.abs(trial - frame_times)) for trial in trial_list]
+frame_count_list = [get_total_frames(file) for file in video_files]
+frame_times_list = [np.linspace(marker_list[0],marker_list[1],frame_count)\
+        for frame_count in frame_count_list]
+frame_inds_list = [[np.argmin(np.abs(trial - frame_times)) for trial in trial_list]\
+                for frame_times in frame_times_list]
 
 # Make a list of tuples to describe start and stop points for every video
-split_markers = [(trial-t_prior_frames,trial+t_post_frames) for trial in frame_inds] 
+split_markers_list = [[(trial-t_prior_frames,trial+t_post_frames) \
+        for trial in frame_inds] \
+        for frame_inds in frame_inds_list]
 
-import dill
-## Save dill session
-dill_filename = directory + '/' + 'dill_session.pkl'
-#dill.dump_session(dill_filename)
+#  ____ _   _  ___  ____     ____ _   _  ___  ____  
+# / ___| | | |/ _ \|  _ \   / ___| | | |/ _ \|  _ \ 
+#| |   | |_| | | | | |_) | | |   | |_| | | | | |_) |
+#| |___|  _  | |_| |  __/  | |___|  _  | |_| |  __/ 
+# \____|_| |_|\___/|_|      \____|_| |_|\___/|_|    
+#                                                   
 
-## Load dill session
-dill.load_session(dill_filename)
-
-
-# CHOP CHOP
-
-cam = 0
-for trial_num in tqdm(range(len(split_markers))):
-    trial = split_markers[trial_num]
-    video_name = video_files[cam]
-    cap = cv2.VideoCapture(video_name)
-    cap.set(1,trial[0])
-    frame_count = trial[0]
-    saved_frame_count = 0
-    success = 1
-    frame_list = []
-    while success and saved_frame_count <= (trial[1]-trial[0]):
-        if frame_count <= trial[1] and frame_count >= trial[0]:
-            success, frame = cap.read()
-            frame_list.append(frame)
-            frame_count += 1 
-            saved_frame_count += 1
-        else:
-            success, frame = cap.read()
-            frame_count += 1 
-    cap.release()
-    
-    # Write out the list of frames at the appropriate framerate
-    resolution = (frame_list[0].shape[1],frame_list[0].shape[0])
-    output_name = directory + '/' + 'trial{}_cam{}.avi'.format(trial_num,cam)
-    writer = cv2.VideoWriter(output_name,
-                           cv2.VideoWriter_fourcc(*'XVID'), 
-                           frame_rate, 
-                           resolution) 
-    
-    for frame in frame_list:
-        writer.write(frame)
-    writer.release()
+for video_num in range(len(video_files)):
+    for trial_num in tqdm(range(len(split_markers_list[video_num]))):
+        trial = split_markers_list[video_num][trial_num]
+        video_name = video_files[video_num]
+        cap = cv2.VideoCapture(video_name)
+        cap.set(1,trial[0])
+        frame_count = trial[0]
+        saved_frame_count = 0
+        success = 1
+        frame_list = []
+        while success and saved_frame_count <= (trial[1]-trial[0]):
+            if frame_count <= trial[1] and frame_count >= trial[0]:
+                success, frame = cap.read()
+                frame_list.append(frame)
+                frame_count += 1 
+                saved_frame_count += 1
+            else:
+                success, frame = cap.read()
+                frame_count += 1 
+        cap.release()
+        
+        # Write out the list of frames at the appropriate framerate
+        resolution = (frame_list[0].shape[1],frame_list[0].shape[0])
+        output_name = directory + '/' + 'trial{}_cam{}.avi'.format(trial_num,video_num)
+        writer = cv2.VideoWriter(output_name,
+                               cv2.VideoWriter_fourcc(*'XVID'), 
+                               frame_rate, 
+                               resolution) 
+        
+        for frame in frame_list:
+            writer.write(frame)
+        writer.release()
 
 
 
-@staticmethod
+# Half-baked functions to clean-up script above (in the future)
+
 def write_module(list_of_frames, output_name, frame_rate):
     """
     list_of_frames :: all frames to be output in a single file
@@ -113,12 +142,11 @@ def write_module(list_of_frames, output_name, frame_rate):
                            cv2.VideoWriter_fourcc('M','J','P','G'), 
                            frame_rate, 
                            resolution) 
-    
+   
     for frame in list_of_frames:
         writer.write(frame)
     writer.release()
 
-@staticmethod
 def split_module(video_capture, split_markers):
     """
     Parses single video file according to markers
